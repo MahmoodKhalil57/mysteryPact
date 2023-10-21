64d08a0b8a8889bb5fc78834841e629beef85eef
MahmoodKhalil57 <mahmoodihabk@gmail.com>
2023-10-21 13:48:20 +0100

    add basic layout

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import Header from '$lib/components/layout/Header.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
+</script>
+
+<div>
+	<Header />
+	<slot />
+	<Footer />
+</div>
